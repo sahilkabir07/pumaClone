76ddf3e01984ddec2d2eb3372a74386df66f4a18
sahilkabir07 <sahilakthar8986@gmail.com>
2025-04-19 17:55:14 +0530

    files changed

--- server/api/server.js
+++ server/api/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/authRoutes");
-const cartRoutes = require("./routes/cartRoutes");
+const authRoutes = require("../routes/authRoutes");
+const cartRoutes = require("../routes/cartRoutes");
 const serverless = require("serverless-http");
+
 dotenv.config();
 
 const app = express();
@@ -12,16 +13,16 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {})
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection failed:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
+
 app.get("/", (req, res) => {
   res.send("hii..");
 });
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
-});
+
+module.exports = app;
 module.exports.handler = serverless(app);
