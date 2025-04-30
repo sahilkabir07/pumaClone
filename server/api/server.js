01905ca491f25abbfb1400ab93a7718a046f45b2
sahilkabir07 <sahilakthar8986@gmail.com>
2025-04-30 09:14:30 +0530

    cors origin changed

--- server/api/server.js
+++ server/api/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://puma-clone-zpmn.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
