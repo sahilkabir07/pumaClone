112f19863e016fa96093aff547e44b231a7cc9b6
sahilkabir07 <sahilakthar8986@gmail.com>
2025-05-06 13:08:04 +0530

    server and vercel files changed

--- server/api/server.js
+++ server/api/server.js
@@ -19,12 +19,21 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
+
+app.options("*", (req, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://puma-clone-zpmn.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  return res.sendStatus(200);
+});
 // CORS options
 const corsOptions = {
   origin: "https://puma-clone-zpmn.vercel.app", // no trailing slash
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 // Apply CORS middleware globally (automatically handles OPTIONS pre-flights)
