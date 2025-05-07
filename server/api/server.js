f0b11b1d32a8f07b9e991101f011b4466de01bc9
sahilkabir07 <sahilakthar8986@gmail.com>
2025-05-07 13:34:17 +0530

    Vercek json File Changed

--- server/api/server.js
+++ server/api/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoutes = require("../routes/authRoutes");
 const cartRoutes = require("../routes/cartRoutes");
-const productRoutes = require("../routes/productRoutes");
 const serverless = require("serverless-http");
-require("dotenv").config();
+
+dotenv.config();
 
 const app = express();
-
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "https://sahil-s-project-cbav.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 mongoose
@@ -18,19 +22,12 @@ mongoose
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection failed:", err));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
-app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  res.send("hi.. server is live 🔥");
+  res.send("hii..");
 });
-
-if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server listening on port ${PORT}`));
-}
 
 module.exports = app;
 module.exports.handler = serverless(app);
