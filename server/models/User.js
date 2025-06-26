f69330d34b8dfc3ea18feb9beb42ec53ec907fde
sahilkabir07 <sahilakthar8986@gmail.com>
2025-06-26 12:13:37 +0530

    user and cart model file changed

--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema, "members");
 
 module.exports = User;
